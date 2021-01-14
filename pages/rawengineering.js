14b20190b1b96c2bbe06b7acdeb51d644dac23f8
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-15 01:45:51 +0530

    Created rawengineering page
    It renders Navigation, Header, Section and footer Component
    sends the data as props to the component

---
+++ pages/rawengineering.js
@@ -0,0 +1,21 @@
+import Navigation from "../components/Navigation";
+import Header from "../components/Header";
+import Section from "../components/Section";
+import Footer from "../components/Footer";
+import styles from "../styles/raw.module.css";
+
+export default function Raw({ data }) {
+  return (
+    <div className={styles["raw"]}>
+      <Navigation styles={styles} navigationLinks={data[0].navigationLinks} />
+      <Header header={data[0].header} styles={styles} />
+      <Section section={data[0].section} styles={styles} />
+      <Footer />
+    </div>
+  );
+}
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/raw");
+  const data = await res.json();
+  return { props: { data } };
+}
